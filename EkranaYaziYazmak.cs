e8905e1ed7d2f4c8e0396518445aaca81449b29b
Şerif Efe DARTAR <dartar.efe@outlook.com.tr>
2020-08-04 19:18:34 +0300

    EkranaYaziYazmak.cs Aktarımı
    Bir Yazılım Diline Girişte İlk Yapılacak İş Olan Ekrana Yazı Yazma İşlemi Ayrıntılı Şekilde İşlenmiştir.

---
+++ EkranaYaziYazmak.cs
@@ -0,0 +1,15 @@
+using System; // Gerekli Kütüphaneler Eklenir.
+
+namespace Github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ekrana Yazılacak Olan Yazı"); // Console İçindeki WriteLine Metodu Kullanarak İlk Satıra Yazılacak Olan Yazı Yazılır.
+            
+            // Ekranın Biz İnceleyene Kadar Açık Kalması İçin 'Console.Readline();' Komutu Kullanılabilir.
+        }
+    }
+}
+
